c884cf3efc4325e3c2ff732ac8d3b97c56ad5871
RayChiuRui <u11302208@o365.ttu.edu.tw>
2025-04-01 21:37:29 +0800

    write P1E1B08_.C example file

---
+++ P1E1B08_.C
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+ #include <stdio.h>
+ int main(void)
+ {	
+	int i,password=2024,inputpassword,ch;  //  設立變數  
+ 	for(i=1; i<=20; i++){   //  for迴圈重複印出表情符號直到20次結束  
+ 		printf("$$$o(∩_∩)o$$$  <（‵□′）>───Cε(┬_┬)3    ⊙⊙⊙ Welcome⊙⊙⊙   (∪ -ω- ∪)-⊙-(∪ -ω- ∪)-⊙-(∪ -ω- ∪)");
+ 		printf("\n");
+ 		}
+	system("PAUSE");  // 螢幕畫面暫停，並等待使用者按任意鍵
+	system("CLS");   // 清除螢幕 
+	printf("請輸入四個數字的密碼\n");
+	scanf("%d",&inputpassword);  // 由鍵盤輸入密碼 
+	if(inputpassword!=password){  // 當輸入的密碼不是預設密碼(2024) 
+		printf("\a");  // 發出警告 
+		printf("密碼輸入有誤\n");  // 顯示輸入有誤 
+		return 0;  // 結束程式 
+	}
+	else printf("歡迎您\n");
+	system("pause");  // 螢幕畫面暫停，並等待使用者按任意鍵
+	system("CLS");  // 清除螢幕
+	printf("-⊙⊙⊙⊙⊙⊙⊙⊙⊙⊙⊙⊙⊙⊙⊙⊙⊙⊙⊙⊙⊙⊙⊙-------\n");
+	printf("⊙ ‘A’...‘Z’ : Uppercase ⊙\n");
+	printf("⊙ ‘a’...‘z’ : Lowercase ⊙\n");
+	printf("⊙ ‘0’...‘9’ : Digit     ⊙\n");
+	printf("⊙ Otherwise : Your name ⊙\n");
+	printf("-⊙⊙⊙⊙⊙⊙⊙⊙⊙⊙⊙⊙⊙⊙⊙⊙⊙⊙⊙⊙⊙⊙⊙-------\n");
+	printf("請輸入 1 個字元\n");
+	scanf("%c",&ch); // 輸入字元 
+	ch=getchar();
+	if((ch>='A')&&(ch<='Z')){  // 如果輸入的字元在範圍(A~Z)間 
+		printf("Uppercase\n");
+		}
+		else if((ch>='a')&&(ch<='z')){  // 如果輸入的字元在範圍(a~z)間  
+			printf("Lowercase\n");
+			}
+			else if((ch>='0')&&(ch<='9')){  // 如果輸入的字元在範圍(0~9)間 
+				printf("Digit\n");
+				}
+				else{ 
+					printf("E1B08 邱睿嘉\n");  //如果輸入範圍不在以上的範圍間 
+					}		 
+	system("pause");  // 螢幕畫面暫停，並等待使用者按任意鍵 
+	return 0;  // 結束程式 
+ }
