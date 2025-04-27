fafcde29bb680a0a1d03aca05ea8939d74f5b3d8
RayChiuRui <u11302208@o365.ttu.edu.tw>
2025-04-28 02:09:01 +0800

    增加了螢幕主選單abc

--- P2E1B08_.c
+++ P2E1B08_.c
@@ -20,4 +20,12 @@ int main(void){
 	}
 	else b++; printf("請輸入四個數字的密碼\n");
 }
+system("PAUSE");/*螢幕畫面暫停，並等待使用者案任意鍵*/ 
+	system("CLS");/*清除螢幕*/
+	fflush(stdin);
+	printf("==========================\n");
+	printf("|| a. 畫出直角三角形 ||\n");
+	printf("|| b. 顯示乘法表     ||\n");
+	printf("|| c. 結束           ||\n");
+	printf("==========================\n");
 }
