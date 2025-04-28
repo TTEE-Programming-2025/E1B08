c0c88fa512fe774735840e06757f0f686e40089b
RayChiuRui <u11302208@o365.ttu.edu.tw>
2025-04-28 22:57:25 +0800

    嘗試將中文亂碼修正

--- P2E1B08_.c
+++ P2E1B08_.c
@@ -2,13 +2,13 @@
 #include<stdlib.h>
 #include<conio.h>
 int main(void){	
-	int i,password=2025,inputpassword,ch,b=0,num,j,k,l;/*設立變數*/ 
+	int i,password=2025,inputpassword,ch,b=0,num,j,k,l; 
 	char c ,ch1,h;
-	for(i=1; i<=20; i++){/*for迴圈重複印出表情符號直到20次結束*/  
+	for(i=1; i<=20; i++){  
  		printf(" (∪ -ω- ∪)-⊙-(∪ -ω- ∪)-⊙-(∪ -ω- ∪)   ⊙⊙⊙ 2025⊙⊙⊙   (∪ -ω- ∪)-⊙-(∪ -ω- ∪)-⊙-(∪ -ω- ∪)");
  		printf("\n");
  		} 
-		printf("輸入四個數字的密碼\n");
+		printf("輸入四個數字密碼\n");
 		printf("提示:2025\n");
 		do{
 		scanf("%d",&inputpassword);
@@ -19,28 +19,28 @@ int main(void){
 		if(b>=2){
 			return 1;
 		}
-			else b++; printf("警告! 請重新輸入四個數字的密碼\n");
+			else b++; printf("警告!，請重新輸入密碼\n");
 		}while(inputpassword!=password);
-		system("PAUSE");/*螢幕畫面暫停，並等待使用者案任意鍵*/ 
-		system("CLS");/*清除螢幕*/
+		system("PAUSE"); 
+		system("CLS");
 		do{
 			fflush(stdin);
 			printf("⊙⊙ ----------⊙⊙----------⊙⊙\n");
-			printf("   | a. 畫出直角三角形 |\n");
+			printf("   |  a. 劃出直角三角形 |\n");
 			printf("   ⊙ b. 顯示乘法表     ⊙\n");
-			printf("   | c. 結束           |\n");
+			printf("   |  c. 結束           |\n");
 			printf("⊙⊙ ----------⊙⊙----------⊙⊙\n");
-			printf("請輸入a or b or c\n");
+			printf("請輸入a or b or c \n");
 			scanf("%c",&c);
 			fflush(stdin);
-			if(c=='a'||c=='A'){//輸入的是否是a 
+			if(c=='a'||c=='A'){ 
 				system("CLS");
 				printf("請輸入a~n字元:");
 				do{
 					scanf("%c",&ch1);
 					fflush(stdin);
 					if(ch1<'a'||ch1>'n'){
-						printf("警告! 請重新輸入a~n的字元:");
+						printf("警告! 請輸入a~n字元");
 					}
 					else{
 						system("CLS");
@@ -54,7 +54,7 @@ int main(void){
 						}
 						printf("\n");
 						}
-						printf("回到主選單\n");
+						printf("回選單\n");
 						getch();
 						system("CLS");
 						break;
@@ -65,12 +65,12 @@ int main(void){
 			}
 			if(c=='b'||c=='B'){
 				system("CLS");
-				printf("輸入一個 1 至 9 的整數 num\n");
+				printf("輸入 1 至 9 的整數 num\n");
 				do{
 					scanf("%d",&num);
 					fflush(stdin);
 					if(num<1||num>9){ 
-						printf("警告! 請重新輸入一個 1 至 9 的整數 num\n");
+						printf("警告! 請重新輸入 1 至 9 的整數 num\n");
 					}
 					else{ 
 						int k,l;
@@ -80,7 +80,7 @@ int main(void){
 							}
 							printf("\n");
 						}
-						printf("回到主選單\n");
+						printf("回選單\n");
 						getch();
 						system("CLS");
 						break;
@@ -89,11 +89,11 @@ int main(void){
 				continue;
 			}
 			if(c=='c'||c=='C'){
-				printf("Countinue? (y/n) :");
+				printf("Continue? (y/n) :");
 				do{
 					scanf("%c",&h);
 					if(h=='y'||c=='Y'){
-						printf("回到主選單\n");
+						printf("回選單\n");
 						system("CLS");
 					}
 					if(h=='n'||c=='N'){
@@ -109,7 +109,7 @@ int main(void){
 				continue;
 				}
 				else {
-					printf("無效數字 錯誤訊息\n");
+					printf("無效數字，錯誤訊息\n");
 					system("pause");
 					system("CLS");
 				}	 
