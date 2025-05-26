c03c465e70925aa4097eff1d3ddbcd07265bdf27
RayChiuRui <u11302208@o365.ttu.edu.tw>
2025-05-26 21:03:01 +0800

    做完個人特色與密碼輸入

---
+++ P4E1B08_.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+	int i,password=2025,inputpassword,ch,b=0,j,k,l;/*設立變數*/ 
+	char c ,ch1,h,num,num1;
+	for(i=1; i<=20; i++){/*for迴圈重複印出表情符號直到20次結束*/  
+ 		printf("(∪ -ω- ∪)-⊙-(∪ -ω- ∪)    ⊙⊙⊙***2025***⊙⊙⊙   (∪ -ω- ∪)-⊙-(∪ -ω- ∪)-⊙-(∪ -ω- ∪)-⊙-(∪ -ω- ∪)");
+ 		printf("\n");
+ 	}
+	printf("請輸入四個數字的密碼\n");
+	printf("提示:2025\n");
+	while(1){
+	scanf("%d",&inputpassword);
+	if(inputpassword==password){
+		break;
+	}
+	if(b>=2){
+		return 1;
+	}
+	else b++; printf("請輸入四個數字的密碼\n");
+}
+	system("PAUSE");/*螢幕畫面暫停，並等待使用者案任意鍵*/ 
+	system("CLS");/*清除螢幕*/
+}
